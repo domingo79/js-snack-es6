73a1ee18476b7c57b8065c261a9f20d63e43e5f3
domingo79 <andria140779@yahoo.it>
2021-05-11 16:33:05 +0200

    snack 1

---
+++ snack-1/main.js
@@ -0,0 +1,19 @@
+/****Snack 1:*****/
+
+//Creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+const BiciDaCorsa = [{ nome: 'Speedmax', peso: 1600 }, { nome: 'Aeroad', peso: 1400 }, { nome: 'Ultimate', peso: 1500 }, { nome: 'Inflite', peso: 1700 }];
+
+console.log(BiciDaCorsa);
+
+// const [, nome] = BiciDaCorsa;
+// console.log(nome.peso);
+
+//Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+const biciLeggera = 1400;
+for (let i = 0; i < BiciDaCorsa.length; i++) {
+    const elem = BiciDaCorsa[i];
+    if (elem.peso <= biciLeggera)
+        document.getElementById('biciPiuma').innerHTML = `
+        <p>La bici più leggera è ${elem.nome}, con i suoi ${elem.peso} gr. </p>
+        `;
+}
