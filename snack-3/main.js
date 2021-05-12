66db653e6d5674a2725b4d6c3c3d32301adf43f3
domingo79 <andria140779@yahoo.it>
2021-05-12 15:11:00 +0200

    funzione

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,7 +1,26 @@
 /*Snack 3:*/
 //Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 const voti = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], bocciato = 5, promosso = 10;
+
+/**
+ * crea un nuovo array con i valori compresi tra min e max
+ * utilizza il metodo filter()
+ * @param {array} array 
+ * @param {numero minimo} min 
+ * @param {numero massimo} max 
+ * @returns 
+ */
+function test(array, min, max) {
+    const nuovoArray = array.filter((elemento) => {
+        if (elemento > min && elemento < max) {
+            return true;
+        }
+        return false;
+    });
+    return nuovoArray;
+};
+console.log(test(voti, bocciato, promosso));
 
 //La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 /* METODO FILTER */
